4103e8afd1004c72179ee9f5102e7c46e1319027
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2019-04-12 09:14:47 -0400

    Updated from Brython Server: 4/12/2019 9:14:46 AM

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 #will try out first concepts here
+from Numpy import *
+
+class NeuralNetwork:
+    def _init_(self,x,y):
+        self.input = x
+        self.weights1 = 2
+        
+random.rand(1,4)
